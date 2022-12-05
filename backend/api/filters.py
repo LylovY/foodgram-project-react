13e18a40ef0eeb570ffb944f8bcfecd0463e5233
Юрий Лылов <lylovY@yandex.ru>
2022-12-05 12:56:14 +0300

    host3

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -14,7 +14,7 @@ class RecipeFilter(django_filters.FilterSet):
         method='filter_is_favorited',
     )
     is_in_shopping_cart = django_filters.CharFilter(
-        method='filter_is_favorited',
+        method='filter_is_in_shopping_cart',
     )
 
     def filter_is_favorited(self, queryset, name, value):
