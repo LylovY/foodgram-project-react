d08f02ec22569ede8503ff7c8fb27ed6609b4a40
Юрий Лылов <lylovY@yandex.ru>
2022-12-05 15:16:08 +0300

    host5

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,6 @@
 from django.db.models import Sum
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
-from django_filters.rest_framework import DjangoFilterBackend
 from recipes.models import Ingredient, Recipe, Tag
 from rest_framework import filters, permissions, status, viewsets
 from rest_framework.decorators import action
